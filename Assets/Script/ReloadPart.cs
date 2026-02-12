0a9a66a04feca7160cef4444d75b58f9679c94a3
Flamme_Vire <noahcoevoet@gmail.com>
2026-02-12 15:46:07 +0100

    Reload don't work

--- Assets/Script/ReloadPart.cs
+++ Assets/Script/ReloadPart.cs
@@ -7,23 +7,49 @@ using UnityEngine.Events;
 public class ManagementPart : MonoBehaviour
 {
     [SerializeField] Part[] parts;
-    Part[] originePart;
+    
     private void Start()
     {
         
+        foreach(var t in parts)
+        {
+            foreach(var d in t.Datas)
+            {
+                d.TilesEventBoolean = d.TileScript.IsEventCallable;
+                d.TilesPosition = d.Tiles.position;
+                d.TilesRotation = d.Tiles.rotation;
+            }
+
+            t.CPAOrigine = (CenterPointAvailable[])t.PAC.CenterPointAvailables.Clone();
+        }
     }
 
     public void ResetPart(int partToSpawn)
     {
-        foreach(var tile in parts)
-        {
 
+        foreach (var d in parts[partToSpawn].Datas)
+        {
+            d.TileScript.IsEventCallable = d.TilesEventBoolean;
+            d.Tiles.position = d.TilesPosition;
+            d.Tiles.rotation = d.TilesRotation;
+            if (parts[partToSpawn].PAC.CurrentTileSelected != null)
+            {
+                parts[partToSpawn].PAC.TryToReleaseTile();
+            }
+            
+                
+            
         }
+        parts[partToSpawn].PAC.CenterPointAvailables = parts[partToSpawn].CPAOrigine;
+
+        
     }
+
     private void Update()
     {
         if (Input.GetButtonDown("Jump"))
         {
+            Debug.Log("index : " + (this.GetComponent<LevelManager>().CurrentPart - 1).ToString());
             ResetPart(this.GetComponent<LevelManager>().CurrentPart - 1);
         }
     }
@@ -33,12 +59,15 @@ public class ManagementPart : MonoBehaviour
 public class Part
 {
     public TileData[] Datas;
+    public PointAndClick PAC;
+    public CenterPointAvailable[] CPAOrigine;
 }
 [Serializable]
 public class TileData
 {
     public Transform Tiles;
-    Vector3 TilesPosition => Tiles.position;
-    Quaternion TilesRotation => Tiles.rotation;
+    [HideInInspector] public Vector3 TilesPosition;
+    [HideInInspector] public Quaternion TilesRotation;
+    [HideInInspector] public bool TilesEventBoolean;
     public Tile TileScript;
 }
